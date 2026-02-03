c4bf1882b4398be1d76f08c79949f7f96d50c8e0
Maksym Huk <maksym.h1009@gmail.com>
2026-02-03 22:57:41 +0200

    le

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const states = [];
+  let currentState = { ...state };
 
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'clear') {
+      newState = {};
+    } else if (action.type === 'addProperties') {
+      newState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      newState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    } else {
+      newState = { ...currentState };
+    }
+
+    states.push(newState);
+    currentState = newState;
+  }
+
+  return states;
+}
 module.exports = transformStateWithClones;
